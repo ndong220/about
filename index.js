98fd0a735c9006a0e425e20bf64ed25e3d99a88e
ndong220 <you@example.com>
2023-04-08 18:06:26 +0700

    Add new project about

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.mobie-icon').addEventListener('click', function() {
+      document.body.classList.toggle("is-mobie");
+    });
+  });
